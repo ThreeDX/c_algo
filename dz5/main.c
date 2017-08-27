96d7633abd44c97861505c4935807d35d68d999a
ThreeD <threed@rsgc.ru>
2017-08-27 15:30:24 +0300

    dz5

--- dz5/main.c
+++ dz5/main.c
@@ -13,52 +13,140 @@
 #define T char
 
 struct Node {
-	T data;
-	struct Node* next;
+    T data;
+    struct Node* next;
 };
 typedef struct Node Node;
 
 typedef struct {
-	Node* head;
-	int size;
+    Node* head;
+    int size;
 } Stack;
 
 void push(Stack* s, T val) {
-	Node* tmp = (Node*) malloc(sizeof(Node));
-	if (tmp == NULL) {
-		printf("Stack Overflow");
-		return;
-	}
-	tmp->data = val;
-	tmp->next = s->head;
-	s->head = tmp;
-	s->size++;
+    Node* tmp = (Node*) malloc(sizeof(Node));
+    if (tmp == NULL) {
+        printf("Stack Overflow");
+        return;
+    }
+    tmp->data = val;
+    tmp->next = s->head;
+    s->head = tmp;
+    s->size++;
 }
 
 T pop(Stack* s) {
-	Node* tmp;
+    Node* tmp;
     T value;
-	if (s->size == 0) {
-		printf("Stack is empty");
-		return -1;
-	}
+    if (s->size == 0) {
+        printf("Stack is empty");
+        return -1;
+    }
     tmp = s->head;
-	value = s->head->data;
-	s->head = s->head->next;
-	free(tmp);
-	s->size--;
-	return value;
+    value = s->head->data;
+    s->head = s->head->next;
+    free(tmp);
+    s->size--;
+    return value;
 }
 
 int length(Stack *s) {
     return s->size;
 }
 
-void printAndClear(Stack *s) {
-    T tmp;
-    while(length(s)) {
-        tmp = pop(s);
-        printf("%d", tmp);
+void clear(Stack *s) {
+    while(length(s))
+        pop(s);
+}
+
+void print(Node *n) {
+    if (n) {
+        printf("%d", n->data);
+        print(n->next);
+    } else
+        return;
+}
+
+/////////////////////////////////////
+// 4. реализовать очередь.
+///////////// Queue /////////////////
+
+struct QNode {
+    T data;
+    struct QNode* next;
+    struct QNode* prev;
+};
+typedef struct QNode QNode;
+
+typedef struct {
+    QNode* head;
+    QNode* tail;
+    int size;
+} Queue;
+
+void pushQ(Queue* q, T val) {
+    QNode* tmp = (QNode*) malloc(sizeof(QNode));
+    if (tmp == NULL) {
+        printf("Stack Overflow");
+        return;
+    }
+    tmp->data = val;
+    tmp->prev = NULL;
+    
+    if (q->head)
+        q->head->prev = tmp;
+    else
+        q->tail = tmp;
+    tmp->next = q->head;
+    q->head = tmp;
+    q->size++;
+}
+
+T popQ(Queue* q) {
+    QNode* tmp;
+    T value;
+    
+    if (q->size == 0) {
+        printf("Queue is empty");
+        return -1;
+    }
+    
+    tmp = q->tail;
+    value = tmp->data;
+    q->tail = tmp->prev;
+    
+    if (q->head == tmp)
+        q->head = q->tail;
+    else
+        q->tail->next = NULL;
+    
+    free(tmp);
+    q->size--;
+    return value;
+}
+
+int lengthQ(Queue *q) {
+    return q->size;
+}
+
+void clearQ(Queue *q) {
+    while(lengthQ(q))
+        popQ(q);
+}
+
+void printQ(QNode *n) {
+    if (n) {
+        printf("%d", n->data);
+        printQ(n->next);
+    } else
+        return;
+}
+
+void fillQueue(Queue* q, int size) {
+    int i = 0;
+    while (i < size) {
+        pushQ(q, i);
+        i++;
     }
 }
 ////////////////////////////
@@ -105,13 +193,36 @@ int checkString(char* str) {
 
     return (s.size == 0);
 }
+
+/*
+ * 3. *Создать функцию, копирующую односвязный список.
+ */
+Node* copy(Node* from) {
+    Node* tmp;
+    if (from) {
+        tmp = (Node*) malloc(sizeof(Node));
+        if (tmp == NULL) {
+            printf("Stack Overflow");
+            return NULL;
+        }
+        tmp->data = from->data;
+        tmp->next = copy(from->next);
+        return tmp;
+    } else
+        return NULL;
+}
+
 /*
 * Main function
 */
 int main(int argc, char* argv[]) {
-   	Stack stack;
-	stack.head = NULL;
-	stack.size = 0;
+    Stack stack;
+    Queue queue;
+    stack.head = NULL;
+    stack.size = 0;
+    queue.head = NULL;
+    queue.tail = NULL;
+    queue.size = 0;
 
     setlocale(LC_ALL, "Russian");
     printf("Dmitry Panfilov Домашняя работа №5\n");
@@ -119,13 +230,26 @@ int main(int argc, char* argv[]) {
     printf("\nЗадание №1: Перевод из 10 сс в двоичную.\n");
     decToBin(100, &stack);
     printf("100 = ");
-    printAndClear(&stack);
+    print(stack.head);
 
-    printf("\nЗадание №2: Проверка скобочной последовательности.\n");
+    printf("\n\nЗадание №2: Проверка скобочной последовательности.\n");
     printf("(2+(2*2)) = %d\n", checkString("(2+(2*2))"));
     printf("[2/{5*(4+7)}] = %d\n", checkString("[2/{5*(4+7)}]"));
     printf("])}) = %d\n", checkString("])})"));
     printf("())({) = %d\n", checkString("())({)"));
+
+    printf("\nЗадание №3: Скопировать список.\n");
+    printf("First = ");
+    print(stack.head);
+    printf("\nSecond = ");
+    print(copy(stack.head));
+
+    printf("\n\nЗадание №4: Реализовать очередь.\n");
+    fillQueue(&queue, 8);
+    printf("Queue = ");
+    printQ(queue.head);
+    printf("\nPopQ = %d, queue = ", popQ(&queue));
+    printQ(queue.head);
 
     getch();
     return 0;
