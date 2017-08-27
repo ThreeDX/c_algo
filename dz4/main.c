825d1f12873b6852be9f634fa9ee1092dc20804d
ThreeD <threed@rsgc.ru>
2017-08-27 10:30:43 +0300

    dz4 fake

--- dz4/main.c
+++ dz4/main.c
@@ -1,73 +1,46 @@
-/*
-* Dmitry Panfilov
-* HomeWork 4
-*/
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <locale.h>
-
+//#include <cs50.h>
 #include <stdio.h>
-#include <stdlib.h>
+//#include <math.h>
 
-void swap(int* a, int* b) {
-    *a ^= *b;
-    *b ^= *a;
-    *a ^= *b;
-}
+int GetNumber();
+int GetCent();
+int getCoins(int nominal, int* pay);
 
-void fillArr(int* arr, int len) {
-    int i;
-    for(i = 0; i < len; i++)
-        arr[i] = rand() % 100 + 1;
-}
-
-void printArr(int* arr, int len) {
-    int i;
-    for(i = 0; i < len; i++)
-        printf("%d ", arr[i]);
-    puts("");
-}
-
-// Вспомогательная функция, Бинарный поиск места в массиве для вставки элемента
-int searchInsertIndex(int* arr, int len, int value, int* count) {
-    int left = 0;
-    int right = len - 1;
-    int middle = left + (right - left) / 2;
-    
-    while (left < right && arr[middle] != value) {
-        (*count)++;
-        if (arr[middle] < value)
-            left = middle + 1;
-        else
-            right = middle - 1;
-        
-        middle = left + (right - left) / 2;
-    }
-    
-    (*count)++;
-    
-    if(arr[middle] < value)
-        return ++middle;
-    else
-        return middle;
-}
-
-/*
-* Main function
-*/
-#define SIZE 25
-int main(int argc, char* argv[]) {
-    int arr[SIZE];
-    srand(time(0));
-    setlocale(LC_ALL, "Russian");
-    printf("Dmitry Panfilov Домашняя работа №4\n");
-    
-    fillArr(arr, SIZE);
-    printArr(arr, SIZE);
-    //printf("MySort count = %d\n", mySort(arr, SIZE));
-    printArr(arr, SIZE);
-
-
+int main(void){
+    printf("%d\n", GetCent());
     getch();
     return 0;
+}
+
+int GetNumber(){
+    float n = 0;
+    do{
+        printf("O hai! How much change is owed?\n");
+        scanf("%f", &n);
+    } while(n < 0);
+    n = n * 100 + 0.5; // 0.5 для корректного округления
+    return (int)n;
+}
+
+int getCoins(int nominal, int* pay) {
+    int coins = 0;
+    coins = *pay / nominal;
+    *pay = *pay % nominal;
+    return coins;
+}
+
+int GetCent(){
+    int quarters = 25;
+    int dimes = 10;
+    int nikels = 5;
+    int pennies = 1;
+    int pay = GetNumber();
+    int coin = 0;
+
+    coin += getCoins(quarters, &pay);
+    coin += getCoins(dimes, &pay);
+    coin += getCoins(nikels, &pay);
+    coin += getCoins(pennies, &pay); // Хотя тут можно просто pay прибавить, номинал 1 к 1
+
+    return coin;
 }
