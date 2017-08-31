d4d4b83307b0c8c2ddf2abbd76461d0ea76ce565
ThreeDX <threed@rsgc.ru>
2017-08-31 22:16:09 +0300

    dz7 init

---
+++ dz7/main.c
@@ -0,0 +1,27 @@
+/*
+* Dmitry Panfilov
+* HomeWork 7
+*/
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <locale.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+* Main function
+*/
+int main(int argc, char* argv[]) {
+
+    setlocale(LC_ALL, "Russian");
+    printf("Dmitry Panfilov Домашняя работа №7\n");
+    
+    /*printf("\nЗадание №1: CRC.\n");
+    printf("\"test string\" = %d\n", crc("test string"));
+	*/
+
+    getch();
+    return 0;
+}
