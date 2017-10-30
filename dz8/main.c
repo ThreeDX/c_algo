ed239ed64d0118f0d72238ab97caaccc0ca897bd
Dmitry <threed@rsgc.ru>
2017-10-30 12:14:17 +0300

    dz8

--- dz8/main.c
+++ dz8/main.c
@@ -58,7 +58,7 @@ int countSort(int *arr, int len)
         for (int j = 0; j < values[i]; ++j)
             arr[k++] = i;
     }
-    count += k + SIZE101;
+    count += len + SIZE101;
 
     free(values);
     return count;
@@ -105,15 +105,17 @@ int quickSort(int *arr, int first, int last)
 /*
 * 3. *Реализовать сортировку слиянием.
 */
+
+
 int mergeSort(int *arr, int left, int right)
 {
     int count = 0;
-    int x = (left + right) / 2;
+    int x = right - left;
 
     if (left < right)
         // Обрабатываемый фрагмент массива состоит более,
         // чем из одного элемента
-        if (right - left == 1) {
+        if (x == 1) {
             ++count;
             if (arr[right] < arr[left])
             {
@@ -123,9 +125,24 @@ int mergeSort(int *arr, int left, int right)
         else
         { // Разбиваем заданный фрагмент на два массива
           // Рекурсивно вызываем функции MergeSort
-            count += mergeSort(arr, left, left + x);
-            count += mergeSort(arr, left + x + 1, right);
-            //Merge(m, l, r); // Сливаем массивы
+            int l = left, r = left + x / 2 + 1;
+            int* temp = (int*)malloc(sizeof(int) * (x + 1));
+            
+            count += mergeSort(arr, left, left + x / 2);
+            count += mergeSort(arr, r, right);
+            
+            // Сливаем массивы, через временный массив
+            for (int i = 0; i < x + 1; i++)
+                if (l <= left + x / 2 && (r > right || arr[l] <= arr[r]))
+                    temp[i] = arr[l++];
+                else
+                    temp[i] = arr[r++];
+
+            for (int i = 0; i < x + 1; i++)
+                arr[left + i] = temp[i];
+
+            count += (x + 1) * 2; // дважды проходим по временному массиву
+            free(temp);
         }
     return count;
 }
@@ -133,9 +150,71 @@ int mergeSort(int *arr, int left, int right)
 /*
 * 4. **Реализовать алгоритм сортировки со списком
 */
-void listSort(int *arr, int len)
-{
+typedef struct Node {
+    int val;
+    struct Node *next;
+} Node;
 
+void add(Node **list, int val)
+{
+    Node *p = (Node*)malloc(sizeof(Node));
+
+    if (p == NULL) {
+        puts("Out of memory!");
+        exit(1);
+    }
+
+    p->val = val;
+    p->next = *list;
+    *list = p;
+}
+
+int pop(Node **list)
+{
+    if (*list == NULL) {
+        //puts("List is NULL");
+        return -1;
+    }
+
+    Node* temp = *list;
+    int val = temp->val;
+    *list = temp->next;
+    free(temp);
+    
+    return val;
+}
+
+// Для упрощения сортируем тот же массив, но сохраняем элементы в массив структур
+int listSort(int *arr, int len)
+{
+    int count = 0; // счетчик обращений к элементам
+    Node **values = (Node**)calloc(SIZE101, sizeof(Node*));
+
+    if (values == NULL) {
+        puts("Out of memory!");
+        exit(1);
+    }
+
+    count += len;
+    for (int i = 0; i < len; ++i) {
+        add(&values[arr[i]], arr[i]);
+    }
+
+    int k = 0;
+    for (int i = 0; i < SIZE101; ++i) {
+        do {
+            int p = pop(&values[i]);
+            if (p >= 0)
+                arr[k++] = p;
+            else
+                break;
+
+        } while (1);
+    }
+    count += len + SIZE101;
+
+    free(values);
+    return count;
 }
 
 /*
@@ -169,21 +248,29 @@ int main(int argc, char* argv[]) {
     printf("\nMergeSort count = %d\n", mergeSort(arr, 0, SIZE - 1));
     printArr(arr, SIZE);
 
-	
+    fillArr(arr, SIZE);
+    //printArr(arr, SIZE);
+    printf("\nListSort count = %d\n", listSort(arr, SIZE));
+    printArr(arr, SIZE);
+
 	fillArr(arr1, SIZE100);
 	fillArr(arr2, SIZE1000);
 	fillArr(arr3, SIZE10000);
 	printf("\n n\t\t\t100\t1000\t10000\n-------------------------------------------------\n");
-//	printf(" Bubble\t\t\t%d\t%d\t%d\n", bubbleSortRC(arr1, SIZE100), bubbleSortRC(arr2, SIZE1000), bubbleSortRC(arr3, SIZE10000));
+	printf(" Count\t\t\t%d\t%d\t%d\n", countSort(arr1, SIZE100), countSort(arr2, SIZE1000), countSort(arr3, SIZE10000));
 	fillArr(arr1, SIZE100);
 	fillArr(arr2, SIZE1000);
 	fillArr(arr3, SIZE10000);
-//	printf(" Shake\t\t\t%d\t%d\t%d\n", shakeSort(arr1, SIZE100), shakeSort(arr2, SIZE1000), shakeSort(arr3, SIZE10000));
+	printf(" Quick\t\t\t%d\t%d\t%d\n", quickSort(arr1, 0, SIZE100 - 1), quickSort(arr2, 0, SIZE1000 - 1), quickSort(arr3, 0, SIZE10000 - 1));
 	fillArr(arr1, SIZE100);
 	fillArr(arr2, SIZE1000);
 	fillArr(arr3, SIZE10000);
-//	printf(" My Sort\t\t%d\t%d\t%d\n", mySort(arr1, SIZE100), mySort(arr2, SIZE1000), mySort(arr3, SIZE10000));
-	printf("-------------------------------------------------\n");
+	printf(" Merge\t\t\t%d\t%d\t%d\n", mergeSort(arr1, 0, SIZE100 - 1), mergeSort(arr2, 0, SIZE1000 - 1), mergeSort(arr3, 0, SIZE10000 - 1));
+    fillArr(arr1, SIZE100);
+    fillArr(arr2, SIZE1000);
+    fillArr(arr3, SIZE10000);
+    printf(" List\t\t\t%d\t%d\t%d\n", listSort(arr1, SIZE100), listSort(arr2, SIZE1000), listSort(arr3, SIZE10000));
+    printf("-------------------------------------------------\n");
 
 
     getch();
